acbe74c41538062c18a1d60af774d7cb734827ee
kiddo_dong <dh655933@gmail.com>
2025-09-05 16:17:14 +0900

    Web Scope

---
+++ BookManagementSystemMVC/src/main/java/com/example/BookManagementSystemMVC/BookManagementSystemMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookManagementSystemMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemMvcApplication.class, args);
+	}
+
+}
---
+++ BookManagementSystemMVC/src/test/java/com/example/BookManagementSystemMVC/BookManagementSystemMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookManagementSystemMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
